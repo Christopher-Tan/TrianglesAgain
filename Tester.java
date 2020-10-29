13a45ea3e8ee75aabf7235ff5de04c59499c9a6b
Christopher Tan <ctan20@stuy.edu>
2020-10-28 23:43:57 -0400

    Added and Tested distanceto method

--- Tester.java
+++ Tester.java
@@ -1,5 +1,7 @@
 public class Tester {
     public static void main(String[] args) {
-        
+        Point v1 = new Point(0,0);
+        Point v2 = new Point(2,2);
+        System.out.println(v1.distanceTo(v2));
     }
 }
