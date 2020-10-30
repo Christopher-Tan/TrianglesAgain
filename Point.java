94c18f7fd350a0ca38546e5652401fc35a590d09
Christopher Tan <ctan20@stuy.edu>
2020-10-30 14:03:50 -0400

    Accounted for null in equals method

--- Point.java
+++ Point.java
@@ -19,6 +19,6 @@ public class Point {
         return Math.sqrt((y - other.y) * (y - other.y) + (x - other.x) * (x - other.x));
     }
     public boolean equals(Point other) {
-        return (x == other.x && y == other.y);
+        return (other != null && x == other.x && y == other.y);
     }
 }
